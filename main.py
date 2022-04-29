b19a537615f8257a6247efee22e45bf19778be56
Pinac Joshi <88838102+PinacJoshi@users.noreply.github.com>
2022-04-30 01:09:48 +0530

    Added pip requirements and Check for Tor Proxy

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-# Python 3.10.4
-import requests
-import os
-import time
+from stem import Signal
+from stem.control import Controller
 
-def main():
-    os.system("sudo systemctl start docker")
-    temp = os.popen("sudo docker run -it -p 9050:9050 -d dperson/torproxy").read()
-    print("Launching Tor Proxy...")
-    time.sleep(2)
-    session = requests.session()
-    session.proxies = {'http':  'socks5h://localhost:9050',
-                       'https': 'socks5h://localhost:9050'}
-    print(session.get('http://httpbin.org/ip').text) # prints {"origin": "67.205.146.164" }
-
-    print(requests.get('http://httpbin.org/ip').text) # prints {"origin": "5.102.254.76" }
-
-    print(session.get('http://galaxy3yrfbwlwo72q3v2wlyjinqr2vejgpkxb22ll5pcpuaxlnqjiid.onion/').text) # Prints the contents of the page
-
-    os.system("sudo docker stop "+temp)
-
-if(__name__ == "__main__"):
-    main()
+controller = Controller.from_port(port=9051)
+controller.authenticate(password="yourpass")
+print(controller.get_info("status/circuit-established"))
