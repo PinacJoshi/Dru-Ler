ddc37d33f21ab68f066b5fcdf67a274ad5ea1019
blitzRahul <104504393+blitzRahul@users.noreply.github.com>
2022-04-30 17:51:03 +0530

    waits for circuit to be established

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from stem import Signal
 from stem.control import Controller
+import time
 
 controller = Controller.from_port(port=9051)
 controller.authenticate(password="yourpass")
 print(controller.get_info("status/circuit-established"))
+while(str(controller.get_info("status/circuit-established"))=="0"):
+  time.sleep(0.5)
+
+#begin
