aadaaf4f135e8895c2893817634aad1012befa20
Rahul <rahul.cmaurya@gmail.com>
2022-05-25 00:03:36 -0700

    added launch.sh

---
+++ launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# packages is an array of required packages
+declare -a packages=("tor" "python3")
+for REQUIRED_PKG in "${packages[@]}"
+do
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+
+if [ "" = "$PKG_OK" ]; then
+  echo "Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+else
+echo "$REQUIRED_PKG is installed"
+fi
+done
+
+#pythonPackages is an array of required python packages
+declare -a pythonpackages=("requests" "PySocks")
+installed=$(pip3 list)
+
+for i in "${pythonpackages[@]}"
+do
+str=$(echo $installed | grep -e "$i")
+
+if [[ -z $str ]]; then
+echo "$i not installed"
+pip install $i
+else
+echo "$i installed"
+fi
+done
+
