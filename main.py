2dc6e6edba7065dcfbf598ddaa55b9716a089882
Rahul <rahul.cmaurya@gmail.com>
2022-05-25 00:52:02 -0700

    main.py starts tor and waits for it to activate

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def starttor():
   global torhash
   torhash = tor.gethash()
   tor.tor(torhash)
+
+
+if __name__ == "__main__":
+
+	process = mp.Process(target=starttor)
+	process.start()
+	while(not tor.checktorcon()):
+		pass
+	print("confirmed bitches")
